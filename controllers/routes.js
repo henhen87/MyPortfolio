55e871db5261909804615ef9a2b3d4e859181579
Owner-PC\hen <btnysci@yahoo.com>
2017-05-21 12:32:07 -0700

    Grammar error

--- controllers/routes.js
+++ controllers/routes.js
@@ -19,8 +19,8 @@ var homeData = {
 			   " I believe my energy and my positive attitude will be a great asset to any company. I enjoy programming so much and I think what separates me from others is, programming is not something I do just for a great salary. Programming is something that I actually enjoy and love doing. " +
 			   " When I am not playing video games or exercising, I am often coding programs and researching new tech and languages during my leisure. It is something very special to me and I will often find myself staying after work hours just to keep programming since it is so much fun, especially with a great team.",
 
-	paragraphTwo: "I have three years of programming experience with C++, JavaScript, Node, Express, MongoDB, MySQL, and React with Redux. I consider myself to be very talented at solving algorithms, and when faced with a really tough one, using resources available to much such as the internet never lets me down." + 
-				  "I am a great team player and am always willing to communicate with other team members and help them out as much as I can when needed. I also always make sure to never get lazy when it comes to writing clean, properly formatted code with best practices.",
+	paragraphTwo: "I have three years of programming experience with C++, JavaScript (es5, es6, es7), Node, Express with RESTful API's, MongoDB, MySQL, React with Redux, HTML/Bootstrap, CSS, Webpack, Babel and many more. I consider myself to be very talented at solving algorithms, and when faced with a really challenging one, using resources available to me such as the internet and whiteboards never lets me down." + 
+				  "My attention to detail is also robust. I am a great team player and am always willing to communicate with other team members and help them out as much as I can when needed. I also always make sure to never get lazy when it comes to writing clean, properly formatted code with best practices.",
 	paragraphThree: "Some of my hobbies are video games, music, cooking and coding.",
 	connectHeader: "Connect with Me",
 	formName: "Name:",
